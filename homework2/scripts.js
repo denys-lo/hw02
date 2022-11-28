17e099e075ea59f8b27de954cf6863a02e91a1cc
denys-lo <div.p.div@gmail.com>
2022-11-28 15:38:15 +0100

    HW 2. Mathematical operations

--- homework2/scripts.js
+++ homework2/scripts.js
@@ -1,5 +1,5 @@
-const getFirstNumber = +prompt("Enter first number:");
-const getSecondNumber = +prompt("Enter second number:");
+const getFirstNumber = +prompt('Enter first number:');
+const getSecondNumber = +prompt('Enter second number:');
 const showResult = `User entered ${getFirstNumber} & ${getSecondNumber}:
 - ${getFirstNumber} + ${getSecondNumber} = ${getFirstNumber + getSecondNumber}
 - ${getFirstNumber} - ${getSecondNumber} = ${getFirstNumber - getSecondNumber}

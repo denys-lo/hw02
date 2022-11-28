0e9aeaaae4ffd8973f261c10ec615336f1bce763
denys-lo <div.p.div@gmail.com>
2022-11-28 15:47:11 +0100

    HW 3. Hours in seconds

--- homework3/scripts.js
+++ homework3/scripts.js
@@ -1,1 +1,4 @@
-const getHours = prompt('Enter')
+const getHours = prompt('Enter numbers of hours:');
+const hoursToSeconds = getHours * 60 * 60;
+const result = `${getHours} hours = ${hoursToSeconds} seconds`;
+alert(result);
